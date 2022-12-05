7e8b8ae9340d0239de627044b5f73575c4440431
Jae <jaekusuma@gmail.com>
2022-12-05 16:50:01 +0700

    cleaning code

--- src/test/java/admin/steps/loginSteps.java
+++ src/test/java/admin/steps/loginSteps.java
@@ -12,30 +12,26 @@ public class loginSteps{
         super();
         this.webDriver = driverSetup.webDriver;
     }
-
+    loginPage loginPage = new loginPage(webDriver);
     @Given("User already on login page")
     public void userAlreadyOnLoginPage(){
-        loginPage ad = new loginPage(webDriver);
-        ad.loginButtonIsDisplayed();
+        loginPage.loginButtonIsDisplayed();
     }
 
     @When("User input {string} as email and {string} as password")
     public void userInputAsEmailAndAsPassword(String arg0, String arg1) {
-        loginPage loginPage = new loginPage(webDriver);
         loginPage.setEmail(arg0);
         loginPage.setPassword(arg1);
     }
 
     @Then("User click login button")
     public void userClickLoginButton() {
-        loginPage loginPage = new loginPage(webDriver);
         loginPage.clickLoginButton();
     }
 
     @And("User redirect to dashboard and show message {string}")
     public void userRedirectToDashboard(String message) throws InterruptedException {
         Thread.sleep(3000);
-        loginPage loginPage = new loginPage(webDriver);
         assertEquals(message, loginPage.successLogin());
         loginPage.clickOkLogin();
     }
